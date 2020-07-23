585975cd997430dbc92a754e358c50bc53827b29
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2020-07-23 11:00:56 +0200

    Added license

--- STM32F446_with_Velleman_VMA412/Core/Src/woof_map.c
+++ STM32F446_with_Velleman_VMA412/Core/Src/woof_map.c
@@ -1,6 +1,47 @@
 /* Picture is from the AdaFruit libraries */
 /* Converted using https://lvgl.io/tools/imageconverter */
 
+/*
+ * 256-color indexed bitmap for using the Velleman VMA412 touchscreen Graphic LCD
+ *
+ * Note: pin assignment for STM32F446 Nucleo Board
+ *
+ * Note: the 8-bit 8080 CPU interface is used.
+
+Software License Agreement (BSD License)
+
+Version: 0.1rc2
+Date: 2020/07/23
+
+Copyright (c) 2020 Jesse op den Brouw.  All rights reserved.
+
+Portions based on :
+ Copyright (c) 2012 Adafruit Industries.  All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+
 #include <stdint.h>
 
 const uint8_t woof_map[] = {
